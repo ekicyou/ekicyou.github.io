374f8e3b9f001a8132a0cf408363c749f95ee629
¦¿å <dot.station@gmail.com>
2014-05-06 12:40:46 +0900

    sync NEKOKO

--- old/dot-sakura/kanri/ghost_data/packUtil/copy_ghost.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ..コピー：ゴースト
-
-set src=%ghost_select%\ghost
-set dst=%work_base_dir%\tmp\ghost
-
-
-call packUtil\copy_sub.bat
--- old/dot-sakura/kanri/ghost_data/packUtil/copy_shell.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ..コピー：シェル
-
-set src=%shell_select%\shell
-set dst=%work_base_dir%\tmp\shell
-
-
-call packUtil\copy_sub.bat
