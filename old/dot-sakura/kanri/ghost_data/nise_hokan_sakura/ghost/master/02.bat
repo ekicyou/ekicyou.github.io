09ddc2ac3ebb772578083e26fae1af10f83194ef
¦¿å <dot.station@gmail.com>
2014-05-06 12:40:55 +0900

    sync NEKOKO

--- old/dot-sakura/kanri/ghost_data/nise_hokan_sakura/ghost/master/02.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo #### ネットワーク更新ファイル作成 ####
-set xcopy_opt=/D:5-1-2004
-cmd /C 00.bat make_network
--- old/dot-sakura/kanri/ghost_data/packUtil/make_nar_balloon.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo ..[nar]ファイル作成処理（バルーン）
-
-pushd %work_tmp_dir%\%balloon_name%
-  zip -9 -q -r %narDir%\balloon_%balloon_name%.nar *.*
-  popd
