7a88a6f123fde2514c99aba9d161456d4803421c
えちょ <dot.station@gmail.com>
2016-06-14 22:08:28 +0900

    sync NEKOKO

--- js/jquery.mousewheel.setting.js
+++ js/jquery.mousewheel.setting.js
@@ -4,23 +4,34 @@ $(function() {
 
     var scroll_webkit = function(mov){
         var el = $('body')
-        var oldPos = el.scrollLeft();
-        var newPos = oldPos - mov * speed;
-        el.scrollLeft(newPos);
+        var oldL = el.scrollLeft();
+        var newL = oldL - mov * speed;
+        el.scrollLeft(newL);
+        var check = el.scrollLeft();
+        return newL === check;
     }
 
-     var scroll_ie = function(mov){
+    var scroll_ie = function(mov){
         var x = window.pageXOffset;
         var oldY = window.pageYOffset;
         var y = oldY + mov * speed;
         window.scrollTo(x,y);
     }
 
+    var scrollFunc = function(mov){
+      var rc = scroll_webkit(mov);
+      if( rc ) scrollFunc = scroll_webkit;
+      else{
+        scrollFunc = scroll_ie;
+        scroll_ie(mov);
+      }
+    }
+
     //マウスホイールで横移動
     $(document).mousewheel(function(event, mov) {
-        scroll_webkit(mov);
-        scroll_ie    (mov);
-
-        return false;   //縦スクロール不可
+      //scroll_webkit(mov);
+      //scroll_ie    (mov);
+      scrollFunc(mov);
+      return false;   //縦スクロール処理を禁ずる
     });
 });
